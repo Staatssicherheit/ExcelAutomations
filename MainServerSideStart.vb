7ceac8181a064b09894b3aecc92b379d7f784c76
Staatssicherheit <141138377+Staatssicherheit@users.noreply.github.com>
2024-05-30 15:25:30 +0900

    Create MainServerSideStart.vb

---
+++ MainServerSideStart.vb
@@ -0,0 +1,20 @@
+Sub ServerSideStart()
+    '서버용에서 돌릴 것
+    
+    Call copyCCPULSE
+    
+    Call LineResult
+    
+    Call LoadRecordingMain
+    
+    Call ovocScreenshot
+    
+End Sub
+
+'1.SBC매크로 workbook 엑세스
+'2.메인시작 클릭
+'3.클릭~ 버튼 클릭
+'4.단말수량 시트 엑세스
+'5.표 복사
+'6.작업중인 파일의 시트에 붙여넣기
+
